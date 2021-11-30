1cb4b6b4da0af9fd99a52b351b42e34b3beb490d
erickjunior555 <erick.lemosjr@bandtec.com.br>
2021-11-30 17:01:02 -0300

    site com api

---
+++ Site-26_11_dashboard_funcionando/api-projeto-site/src/models/medidaModel.js
@@ -0,0 +1,33 @@
+var database = require("../database/config");
+
+
+function buscarUltimasMedidas(idBarril, limite_linhas) {
+    instrucaoSql = `select 
+                        idSensor,
+                        temperatura_lm35, 
+                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+                        from leitura join sensor on idSensor = 200
+                        order by idLeitura desc limit ${limite_linhas};`;
+
+    console.log("Executando a instrução SQL: \n"+instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+function buscarMedidasEmTempoReal(idBarril) {
+    instrucaoSql = `select 
+    idSensor,
+    temperatura_lm35, 
+    DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+    from leitura join sensor on idSensor = 200;
+`;
+                    
+    console.log("Executando a instrução SQL: \n"+instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    buscarUltimasMedidas,
+    buscarMedidasEmTempoReal
+}
