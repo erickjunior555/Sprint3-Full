1d818330cd0c806ef8aed8e34e2a5d3f706d190a
erickjunior555 <erick.lemosjr@bandtec.com.br>
2021-12-02 10:58:40 -0300

    att

--- site-dashboard/api-nosso-sensor/api-sensor/node_lm35/app/database.js
+++ site-dashboard/api-nosso-sensor/api-sensor/node_lm35/app/database.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     port     : '3306',
-    user     : 'user2',
-    password : '#clientesenha',
+    user     : 'root',
+    password : 'password',
     database : 'termovinhos'
 });
 
